12f01dbfd844defac0ba29b2cca5a0efb0a456fc
tanu kumari <tanukumarisinha.123@gmail.com>
2022-09-02 17:46:40 +0530

    apply-query

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let container = document.querySelector("#container");
+let dino = document.querySelector("#dino");
+let block = document.querySelector("#block");
+let road = document.querySelector("#road");
+let cloud = document.querySelector("#cloud");
+let score = document.querySelector("#score");
+let gameOver = document.querySelector("#gameOver");
+
+//declaring variable for score
+let interval = null;
+let playerScore = 0;
