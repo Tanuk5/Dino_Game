ab38894b47587e4497ebe373042c32ff30e3d8d0
tanu kumari <tanukumarisinha.123@gmail.com>
2022-09-03 14:59:50 +0530

    dino_jump

--- js/script.js
+++ js/script.js
@@ -16,4 +16,53 @@ let scoreCounter = ()=>{
     score.innerHTML = `Score <b>${playerScore}</b>`;
 }
 
- interval =setInterval(scoreCounter, 200);
+// start Game
+window.addEventListener("keydown" , (start) => {
+    // console.log(start);
+     if(start.code == "Space")
+      {
+        gameOver.style.display = "none"; 
+        block.classList.add("blockActive");
+        road.firstElementChild.style.animation = "roadAnimate 1.5s linear infinite";
+        cloud.firstElementChild.style.animation = "cloudAnimate 50s linear infinite";
+        
+        //score
+        let playerScore = 0;
+        interval = setInterval(scoreCounter,200);
+      }
+        
+});
+
+// jump your dino
+window.addEventListener("keydown", (e)=>{
+// console.log(e);
+
+    if(e.key == "ArrowUp")
+      if(dino.classList != "dinoActive")
+       {
+        dino.classList.add("dinoActive");
+
+        // remove class after 0.5 seconds
+        setTimeout(()=>{
+            dino.classList.remove("dinoActive");
+        }, 500);
+    }
+});
+//GaeOver if dino hit the block
+let result = setInterval(()=>{
+    let dinoBottom = parseInt(getComputedStyle(dino).getPropertyValue("Bottom"));
+    // console.log("dinoBottom" + dinoBottom);
+
+    let blockLeft = parseInt(getComputedStyle(block).getPropertyValue("Left"));
+    // console.log("blockLeft" + blockLeft);
+
+    if(dinoBottom <= 90 && blockLeft >= 20 && blockLeft <= 145){
+    // console.log("Game Over");
+       gameOver.style.display = "block";
+       block.classList.remove("blockActive");
+       road.firstElementChild.style.animation = "none";
+       cloud.firstElementChild.style.animation = "none";
+       clearInterval(interval);
+       playerScore = 0;
+    }
+}, 10);
